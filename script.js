343a9479c48be8d5ca38c32eb16bd3b518297bac
Pranshu Agrawal <101993746+Pranshu-15@users.noreply.github.com>
2024-05-21 20:17:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
     const hamburger = document.getElementById("ham");
-    const navLinks = document.querySelector(".nav-links");
+    const navLinks = document.querySelector(".nav-sub");
 
     hamburger.addEventListener("click", function() {
         navLinks.classList.toggle("show");
