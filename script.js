78226349332e2a2eb3564e44ac751b5339d505cc
Pranshu Agrawal <101993746+Pranshu-15@users.noreply.github.com>
2024-05-21 18:29:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+    const hamburger = document.getElementById("hamburger");
+    const navLinks = document.querySelector(".nav-links");
+
+    hamburger.addEventListener("click", function() {
+        navLinks.classList.toggle("show");
+    });
+});
